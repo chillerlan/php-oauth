dc92fe46a6b0436a687baf7827edc5f978e4c21d
smiley <codemasher@users.noreply.github.com>
2024-07-23 18:08:50 +0200

    :octocat: Spotify playlist-diff example: add merge method

--- examples/Providers/Spotify/playlist-diff.php
+++ examples/Providers/Spotify/playlist-diff.php
@@ -13,20 +13,36 @@ require_once __DIR__.'/spotify-common.php';
 
 class PlaylistDiff extends SpotifyClient{
 
-	public function diff(string $playlistID1, string $playlistID2, bool $createAsPlaylist = false):array{
+	public function diff(string $playlistID1, string $playlistID2):array{
 		$p1   = array_keys($this->getPlaylist($playlistID1));
 		$p2   = array_keys($this->getPlaylist($playlistID2));
 		$diff = array_diff($p1, $p2);
 
-		if($createAsPlaylist){
-			$playlistID = $this->createPlaylist(
-				'playlist diff',
-				sprintf('diff between playlists "spotify:playlist:%s" and "spotify:playlist:%s"', $playlistID1, $playlistID2)
-			);
-			$this->addTracks($playlistID, $diff);
-		}
+		$playlistID = $this->createPlaylist(
+			'playlist diff',
+			sprintf('diff between playlists "spotify:playlist:%s" and "spotify:playlist:%s"', $playlistID1, $playlistID2)
+		);
+
+		$this->addTracks($playlistID, $diff);
 
 		return $diff;
+	}
+
+	public function merge(string $targetID, string ...$playlistIDs):array{
+		$merged = $this->getPlaylist($targetID);
+
+		foreach($playlistIDs as $playlistID){
+			$merged = array_merge($merged, $this->getPlaylist($playlistID));
+		}
+
+		$playlistID = $this->createPlaylist(
+			'playlist merge',
+			sprintf('merged playlists "%s" into "spotify:playlist:%s"', implode('", "', $playlistIDs), $targetID)
+		);
+
+		$this->addTracks($playlistID, array_keys($merged));
+
+		return $merged;
 	}
 
 }
@@ -37,4 +53,5 @@ class PlaylistDiff extends SpotifyClient{
  */
 
 $spotify = $factory->getProvider(PlaylistDiff::class, OAuthExampleProviderFactory::STORAGE_FILE);
-$spotify->diff('37i9dQZF1DX4UtSsGT1Sbe', '37i9dQZF1DXb57FjYWz00c', true);
+$spotify->diff('37i9dQZF1DX4UtSsGT1Sbe', '37i9dQZF1DXb57FjYWz00c');
+$spotify->merge('37i9dQZF1DX4UtSsGT1Sbe', '37i9dQZF1DXb57FjYWz00c');

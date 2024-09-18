55ebdd417216189920340ad5a9b893e277ca723b
smiley <codemasher@users.noreply.github.com>
2024-09-18 21:06:35 +0200

    :octocat: i hate this

--- .phpstan/ignore-by-php-version.php
+++ .phpstan/ignore-by-php-version.php
@@ -4,7 +4,10 @@ declare(strict_types = 1);
 
 $includes = [];
 
-if(PHP_VERSION_ID < 80300){
+if(PHP_VERSION_ID < 80200){
+	$includes[] = __DIR__.'/baseline-lt-8.2.neon';
+}
+elseif(PHP_VERSION_ID < 80300){
 	$includes[] = __DIR__.'/baseline-lt-8.3.neon';
 }
 
